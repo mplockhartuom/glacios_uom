b55e3a6f86a3569a7c94bf3b950dbfc6becb8069
Mike Lockhart <michael.lockhart@manchester.ac.uk>
2020-12-08 14:33:08 +0000

    streamlined

--- relion_watcher.bash
+++ relion_watcher.bash
@@ -33,31 +33,6 @@ else
   exit
 fi
 
-
-
-# Make a list of existing mrc files and start converting them
-
-###
-### Clean this section up
-###
-current_dir=$PWD
-cd $collection_dir
-
-if [[ "ls *.mrc"  < "5" ]] ;
-	then
-	sleep 10
-	echo "Waiting for gain"
-fi
-
-ls */*/*.mrc | head -5 > 5_files_gain.lst
-relion_convert_to_tiff --i 5_files_gain.lst --estimate_gain --o $dest_dir
-
-gain_estimate=$dest_dir/gain_estimate.bin
-
-ls *.mrc > existing_files.lst
-relion_convert_to_tiff --i existing_files.lst --gain $gain_estimate --o $dest_dir --only_do_unfinished &
-cd $current_dir
-
 # start running the watching script for new  Linear file format files.
 inotifywait -mr --event create --event moved_to --format '%e %w%f' $collection_dir | while read ACTION FILE
   do
